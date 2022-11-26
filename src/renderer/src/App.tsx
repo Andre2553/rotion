6fd79fbb2be9c5d97421ddecc5ee99b72be9f426
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-11-26 22:13:03 +1100

    components Header ...

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,17 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import './styles/global.css'
 
 export default function App() {
   return(
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100">
-
+    <div className="h-screen w-screen text-rotion-100 flex">
+      <Sidebar/>
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header/>
+        <main className="flex-1 flex items-center justify-center text-rotion-400">
+          Select or Create a new document
+        </main>
+      </div>
     </div>
   )
 }
