6372dbd1d34d13c0a4839d47f9a827617c710e09
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-11-29 16:02:40 +1100

    Update ipc.ts

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,22 +1,61 @@
 import { ipcMain } from "electron";
 import { IPC } from "../renderer/src/shared/constants/ipc";
-import { FetchAllDocumentsResponse } from "../renderer/src/shared/types/ipc";
+import { randomUUID } from "node:crypto";
+import {
+  CreateDocumentResponse,
+  DeleteDocumentRequest,
+  Document,
+  FetchAllDocumentsResponse,
+  FetchDocumentRequest,
+  FetchDocumentResponse,
+  SaveDocumentRequest,
+} from "../renderer/src/shared/types/ipc";
+import { store } from "./store";
 ipcMain.handle(
   IPC.DOCUMENTS.FETCH_ALL,
   async (): Promise<FetchAllDocumentsResponse> => {
     return {
-      data: [
-        {
-          id: "1",
-          title: "Document 1",
-          content: "Document 1 content",
-        },
-        {
-          id: "2",
-          title: "Document 2",
-          content: "Document 2 content",
-        },
-      ],
+      data: Object.values(store.get("documents")),
     };
+  }
+);
+
+ipcMain.handle(
+  IPC.DOCUMENTS.FETCH,
+  async (_, { id }: FetchDocumentRequest): Promise<FetchDocumentResponse> => {
+    const document = store.get(`documents.${id}`) as Document;
+    return {
+      data: document,
+    };
+  }
+);
+
+ipcMain.handle(
+  IPC.DOCUMENTS.CREATE,
+  async (): Promise<CreateDocumentResponse> => {
+   const id = randomUUID();
+   const document: Document = {
+      id,
+      title: "Untitled",
+   }
+    store.set(`documents.${id}`, document);
+    return {
+      data: document,
+    };
+  }
+);
+
+ipcMain.handle(
+  IPC.DOCUMENTS.SAVE,
+  async (_, { id, title, content}: SaveDocumentRequest): Promise<void> => {
+    store.set(`documents.${id}`, { id, title, content });
+  }
+);
+
+ipcMain.handle(
+  IPC.DOCUMENTS.DELETE,
+  async (_, { id }: DeleteDocumentRequest): Promise<void> => {
+    //@ts-ignore
+    store.delete(`documents.${id}`);
   }
 );
