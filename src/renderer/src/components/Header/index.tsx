4939776047b2f7be6ce8e88026f38b5d36836a04
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-11-30 14:06:39 +1100

    deletion of docs dev

--- src/renderer/src/components/Header/index.tsx
+++ src/renderer/src/components/Header/index.tsx
@@ -2,12 +2,29 @@ import clsx from 'clsx'
 import { Code, CaretDoubleRight, TrashSimple } from 'phosphor-react'
 import * as Breadcrumbs from './Breadcrumbs'
 import * as Collapsible from '@radix-ui/react-collapsible';
+import { useParams } from 'react-router-dom';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Document } from '../../shared/types/ipc';
 
 interface HeaderProps{
   isSideBarOpen: boolean;
 }
 
 export function Header(props: HeaderProps) {
+
+  const { id } = useParams<{ id: string }>();
+  const queryClient = useQueryClient();
+  const { mutateAsync: deleteDocument, isLoading: isDeletingDocument} = useMutation( async () => {
+     await window.api.deleteDocument({ id: id! });
+  },
+  {
+    onSuccess: () => {
+      queryClient.setQueryData<Document[]>(['documents'], (documents) => {
+        return documents?.filter((documents) => documents.id !== id);
+      })
+    }
+  });
+
   const isMacOS = process.platform === 'darwin'
   const isSidebarOpen = props.isSideBarOpen;
 
@@ -15,7 +32,7 @@ export function Header(props: HeaderProps) {
     <div
       id="header"
       className={clsx(
-        'border-b border-rotion-600 py-[1.125rem] px-6 flex items-center gap-4 leading-tight transition-all duration-250 region-drag',
+        'border-b h-14 border-rotion-600 py-[1.125rem] px-6 flex items-center gap-4 leading-tight transition-all duration-250 region-drag',
         {
           'pl-24': !isSidebarOpen && isMacOS,
           'w-screen': !isSidebarOpen,
@@ -47,7 +64,10 @@ export function Header(props: HeaderProps) {
         </Breadcrumbs.Root>
 
         <div className="inline-flex region-no-drag">
-          <button className="inline-flex items-center gap-1 text-rotion-100 text-sm hover:text-rotion-50">
+          <button 
+          onClick={() => deleteDocument()}
+          disabled={isDeletingDocument}
+          className="inline-flex items-center gap-1 text-rotion-100 text-sm hover:text-rotion-50 disabled:opacity-60">
             <TrashSimple className="h-4 w-4" />
             Delete
           </button>
--- src/renderer/src/components/Sidebar/Navigation/Link.tsx
+++ src/renderer/src/components/Sidebar/Navigation/Link.tsx
@@ -12,9 +12,14 @@ export function Link({ to, children }: LinkProps) {
   return (
     <NavLink
       to={to}
-      className={clsx(
-        'flex items-center text-sm gap-2 text-rotion-100 hover:text-rotion-50 py-1 px-3 rounded group hover:bg-rotion-700',
-      )}
+      className={({ isActive }) =>{
+        return clsx(
+          'flex items-center text-sm gap-2 text-rotion-100 hover:text-rotion-50 py-1 px-3 rounded group hover:bg-rotion-700',
+          {
+            'bg-rotion-700': isActive,
+          }
+        )
+      }}
     >
       <span className="truncate flex-1">{children}</span>
 
